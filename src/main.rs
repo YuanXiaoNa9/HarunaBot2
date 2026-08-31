9e814fefc72852c6ebdc640946aa18a70653e237
TsukiMi <yuanxiaona9@outlook.com>
2026-08-31 11:24:05 +0800

    修复有可能出现的消息发送失败，实际却成功的情况，主要原因为使用了代理服务器，解决方法是将client设置为no——proxy

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use reqwest::Client;
 use std::sync::LazyLock;
 static PATH: LazyLock<String> = LazyLock::new(|| env::current_dir().unwrap().display().to_string());
 static MAIN_CONFIG: LazyLock<MainConfig> = LazyLock::new(|| get_config());
-static HTTP_CLIENT: LazyLock<Client> = LazyLock::new(|| Client::new());
+static HTTP_CLIENT: LazyLock<Client> = LazyLock::new(|| Client::builder().no_proxy().build().unwrap());
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     //初始化日志
--- src/msg_sys/msg_reply.rs
+++ src/msg_sys/msg_reply.rs
@@ -1,5 +1,5 @@
 use crate::msg_sys::msg_reply::Data::{At, Face, File, Image, Node, Record, Reply, Video};
-use crate::msg_sys::msg_sys::{Msg, MsgHandler};
+use crate::msg_sys::msg_sys::{Msg};
 use crate::{HTTP_CLIENT, MAIN_CONFIG};
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
@@ -136,7 +136,7 @@ impl SendMsg {
                 debug!("server return:{}", i.status());
             }
             Err(e) => {
-                error!("消息发送失败{}", e)
+                error!("消息发送失败{:?}", e);
             }
         };
     }
